GameKyuubi/MPD

commit 5c598eb949680e2dc6846db9e5c7a6d21d548c73

db/upnp/Directory: parse item_class in StartElement
Don't add to the std::map.

src/db/upnp/Directory.cxx
@@ -32,7 +32,6 @@ static const char *const upnptags[] = {
 	"upnp:album",
 	"upnp:genre",
 	"upnp:originalTrackNumber",
-	"upnp:class",
 	nullptr,
 };
 
@@ -105,6 +104,12 @@ protected:
 				const char *pid = GetAttribute(attrs, "parentID");
 				if (pid != nullptr)
 					m_tobj.m_pid = pid;
+
+				const char *item_class_name =
+					GetAttribute(attrs, "upnp:class");
+				if (item_class_name != nullptr)
+					m_tobj.item_class =
+						ParseItemClass(item_class_name);
 			}
 			break;
 
@@ -126,18 +131,9 @@ protected:
 
 	bool checkobjok() {
 		if (m_tobj.m_id.empty() || m_tobj.m_pid.empty() ||
-		    m_tobj.m_title.empty())
+		    m_tobj.m_title.empty() ||
+		    m_tobj.item_class == UPnPDirObject::ItemClass::UNKNOWN)
 			return false;
-
-		if (m_tobj.type == UPnPDirObject::Type::ITEM) {
-			const char *item_class_name =
-				m_tobj.m_props["upnp:class"].c_str();
-			auto item_class = ParseItemClass(item_class_name);
-			if (item_class == UPnPDirObject::ItemClass::UNKNOWN)
-				return false;
-
-			m_tobj.item_class = item_class;
-		}
 
 		return true;
 	}

commit d08406ae6c63195950c94fc7f20841d1ad2d4ea2

db/upnp: fix "upnp:class" parser
"upnp:class" is an element, not an attribute of element "item".  This
fixes a regression from commit 5c598eb9

src/db/upnp/Directory.cxx
@@ -104,12 +104,6 @@ protected:
 				const char *pid = GetAttribute(attrs, "parentID");
 				if (pid != nullptr)
 					m_tobj.m_pid = pid;
-
-				const char *item_class_name =
-					GetAttribute(attrs, "upnp:class");
-				if (item_class_name != nullptr)
-					m_tobj.item_class =
-						ParseItemClass(item_class_name);
 			}
 			break;
 
@@ -132,7 +126,8 @@ protected:
 	bool checkobjok() {
 		if (m_tobj.m_id.empty() || m_tobj.m_pid.empty() ||
 		    m_tobj.m_title.empty() ||
-		    m_tobj.item_class == UPnPDirObject::ItemClass::UNKNOWN)
+		    (m_tobj.type == UPnPDirObject::Type::ITEM &&
+		     m_tobj.item_class == UPnPDirObject::ItemClass::UNKNOWN))
 			return false;
 
 		return true;
@@ -168,6 +163,11 @@ protected:
 			}
 			break;
 		case 'u':
+			if (m_path.back() == "upnp:class") {
+				m_tobj.item_class = ParseItemClass(str.c_str());
+				break;
+			}
+
 			for (auto i = upnptags; *i != nullptr; ++i)
 				if (!m_path.back().compare(*i))
 					m_tobj.m_props[*i] += str;