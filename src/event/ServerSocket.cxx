GameKyuubi/MPD

commit 075f523ed199e997da65ecca956f3d3def1f55f1

event/ServerSocket: use AllocatedSocketAddress to build local socket address

src/event/ServerSocket.cxx
@@ -429,21 +429,12 @@ bool
 ServerSocket::AddPath(AllocatedPath &&path, Error &error)
 {
 #ifdef HAVE_UN
-	struct sockaddr_un s_un;
+	(void)error;
 
-	const size_t path_length = path.length();
-	if (path_length >= sizeof(s_un.sun_path)) {
-		error.Set(server_socket_domain,
-			  "UNIX socket path is too long");
-		return false;
-	}
+	AllocatedSocketAddress address;
+	address.SetLocal(path.c_str());
 
-	RemoveFile(path);
-
-	s_un.sun_family = AF_UNIX;
-	memcpy(s_un.sun_path, path.c_str(), path_length + 1);
-
-	OneServerSocket &s = AddAddress({(const sockaddr *)&s_un, sizeof(s_un)});
+	OneServerSocket &s = AddAddress(std::move(address));
 	s.SetPath(std::move(path));
 
 	return true;

commit 2d01e139e54b5b082b2b1e4d39bac2a6d8e7b705

event/ServerSocket: fix AF_UNIX address in use error
bind fails with an "address already in use" error if the socket path
already exists.

This was broken by 075f523e.

src/event/ServerSocket.cxx
@@ -431,6 +431,8 @@ ServerSocket::AddPath(AllocatedPath &&path, Error &error)
 #ifdef HAVE_UN
 	(void)error;
 
+	RemoveFile(path);
+
 	AllocatedSocketAddress address;
 	address.SetLocal(path.c_str());
 