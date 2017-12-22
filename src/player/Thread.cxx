GameKyuubi/MPD

commit 36b1697786e31ad7a34182778f2bb5a433ccd59f

player/Thread: move mutex lock out of StartDecoder()

src/player/Thread.cxx
@@ -183,7 +183,7 @@ private:
 	/**
 	 * Start the decoder.
 	 *
-	 * Player lock is not held.
+	 * Caller must lock the mutex.
 	 */
 	void StartDecoder(MusicPipe &pipe) noexcept;
 
@@ -360,8 +360,6 @@ Player::StartDecoder(MusicPipe &_pipe) noexcept
 {
 	assert(queued || pc.command == PlayerCommand::SEEK);
 	assert(pc.next_song != nullptr);
-
-	const std::lock_guard<Mutex> protect(pc.mutex);
 
 	/* copy ReplayGain parameters to the decoder */
 	dc.replay_gain_mode = pc.replay_gain_mode;
@@ -630,9 +628,9 @@ Player::SeekDecoder() noexcept
 		   pipe */
 		pipe->Clear(buffer);
 
+		const std::lock_guard<Mutex> lock(pc.mutex);
 		/* re-start the decoder */
 		StartDecoder(*pipe);
-		const std::lock_guard<Mutex> lock(pc.mutex);
 		ActivateDecoder();
 
 		if (!WaitDecoderStartup())
@@ -699,11 +697,8 @@ Player::ProcessCommand() noexcept
 		queued = true;
 		pc.CommandFinished();
 
-		{
-			const ScopeUnlock unlock(pc.mutex);
-			if (dc.LockIsIdle())
-				StartDecoder(*new MusicPipe());
-		}
+		if (dc.IsIdle())
+			StartDecoder(*new MusicPipe());
 
 		break;
 
@@ -981,8 +976,9 @@ Player::Run() noexcept
 {
 	pipe = new MusicPipe();
 
-	StartDecoder(*pipe);
 	pc.Lock();
+
+	StartDecoder(*pipe);
 	ActivateDecoder();
 
 	pc.state = PlayerState::PLAY;

commit d1ecde6804a70b0254857860bdd8d3087ce91d76

player/Thread: add missing lock for StartDecoder()
Regression from commit 36b1697786e31ad7a34182778f2bb5a433ccd59f

src/player/Thread.cxx
@@ -1042,6 +1042,7 @@ Player::Run() noexcept
 
 			assert(dc.pipe == nullptr || dc.pipe == pipe);
 
+			const std::lock_guard<Mutex> lock(pc.mutex);
 			StartDecoder(*new MusicPipe());
 		}
 