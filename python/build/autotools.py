felixhaedicke/XCSoar

build/python: refactoring: introduce new class MakeProject
This introduces a the new class MakeProject, which is used as a base
class for all Makefile based thirdparty libraries.

build/python/build/autotools.py
@@ -1,8 +1,8 @@
 import os.path, subprocess, sys
 
-from build.project import Project
+from build.makeproject import MakeProject
 
-class AutotoolsProject(Project):
+class AutotoolsProject(MakeProject):
     def __init__(self, url, alternative_url, md5, installed, configure_args=[],
                  autogen=False,
                  cppflags='',
@@ -14,7 +14,7 @@ class AutotoolsProject(Project):
                  use_destdir=False,
                  make_args=[],
                  **kwargs):
-        Project.__init__(self, url, alternative_url, md5, installed, **kwargs)
+        MakeProject.__init__(self, url, alternative_url, md5, installed, **kwargs)
         self.configure_args = configure_args
         self.autogen = autogen
         self.cppflags = cppflags
@@ -22,7 +22,6 @@ class AutotoolsProject(Project):
         self.libs = libs
         self.shared = shared
         self.install_prefix = install_prefix
-        self.install_target = install_target
         self.use_destdir = use_destdir
         self.make_args = make_args
 
@@ -81,14 +80,15 @@ class AutotoolsProject(Project):
         subprocess.check_call(configure, cwd=build, env=toolchain.env)
         return build
 
+    def get_make_args(self, toolchain):
+        return MakeProject.get_make_args(self, toolchain) + self.make_args
+
+    def get_make_install_args(self, toolchain):
+        args = MakeProject.get_make_install_args(self, toolchain)
+        if self.use_destdir:
+            args += ['DESTDIR=' + toolchain.install_prefix]
+        return args
+
     def build(self, toolchain):
         build = self.configure(toolchain)
-
-        destdir = []
-        if self.use_destdir:
-            destdir = ['DESTDIR=' + toolchain.install_prefix]
-
-        subprocess.check_call(['/usr/bin/make', '--quiet', '-j12'] + self.make_args,
-                              cwd=build, env=toolchain.env)
-        subprocess.check_call(['/usr/bin/make', '--quiet', self.install_target] + destdir,
-                              cwd=build, env=toolchain.env)
+        MakeProject.build(self, toolchain, build)

GameKyuubi/MPD

python/build/autotools.py
@@ -1,24 +1,22 @@
 import os.path, subprocess, sys
 
-from build.project import Project
+from build.makeproject import MakeProject
 
-class AutotoolsProject(Project):
+class AutotoolsProject(MakeProject):
     def __init__(self, url, md5, installed, configure_args=[],
                  autogen=False,
                  cppflags='',
                  ldflags='',
                  libs='',
-                 install_target='install',
                  **kwargs):
-        Project.__init__(self, url, md5, installed, **kwargs)
+        MakeProject.__init__(self, url, md5, installed, **kwargs)
         self.configure_args = configure_args
         self.autogen = autogen
         self.cppflags = cppflags
         self.ldflags = ldflags
         self.libs = libs
-        self.install_target = install_target
 
-    def build(self, toolchain):
+    def configure(self, toolchain):
         src = self.unpack(toolchain)
         if self.autogen:
             if sys.platform == 'darwin':
@@ -49,7 +47,8 @@ class AutotoolsProject(Project):
         ] + self.configure_args
 
         subprocess.check_call(configure, cwd=build, env=toolchain.env)
-        subprocess.check_call(['/usr/bin/make', '--quiet', '-j12'],
-                              cwd=build, env=toolchain.env)
-        subprocess.check_call(['/usr/bin/make', '--quiet', self.install_target],
-                              cwd=build, env=toolchain.env)
+        return build
+
+    def build(self, toolchain):
+        build = self.configure(toolchain)
+        MakeProject.build(self, toolchain, build)