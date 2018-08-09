GameKyuubi/MPD

commit 0965524d0a397b776aec2bbb2b10fae50b34b0c4

net/ServerSocket: pass UniqueSocketDescriptor&& to OnAccept()

src/event/ServerSocket.cxx
@@ -150,7 +150,7 @@ inline void
 OneServerSocket::Accept() noexcept
 {
 	StaticSocketAddress peer_address;
-	auto peer_fd = Get().AcceptNonBlock(peer_address);
+	UniqueSocketDescriptor peer_fd(Get().AcceptNonBlock(peer_address));
 	if (!peer_fd.IsDefined()) {
 		const SocketErrorMessage msg;
 		FormatError(server_socket_domain,
@@ -165,7 +165,7 @@ OneServerSocket::Accept() noexcept
 			    (const char *)msg);
 	}
 
-	parent.OnAccept(peer_fd.Get(), peer_address,
+	parent.OnAccept(std::move(peer_fd), peer_address,
 			get_remote_uid(peer_fd.Get()));
 }
 

src/Listen.cxx
@@ -50,9 +50,10 @@ public:
 		:ServerSocket(_loop), partition(_partition) {}
 
 private:
-	void OnAccept(int fd, SocketAddress address, int uid) override {
+	void OnAccept(UniqueSocketDescriptor &&fd,
+		      SocketAddress address, int uid) override {
 		client_new(GetEventLoop(), partition,
-			   SocketDescriptor(fd), address, uid);
+			   std::move(fd), address, uid);
 	}
 };
 

src/client/Client.hxx
@@ -38,6 +38,7 @@
 #include <stddef.h>
 
 class SocketAddress;
+class UniqueSocketDescriptor;
 class EventLoop;
 class Path;
 struct Instance;
@@ -94,7 +95,7 @@ public:
 	std::list<ClientMessage> messages;
 
 	Client(EventLoop &loop, Partition &partition,
-	       SocketDescriptor fd, int uid, int num);
+	       UniqueSocketDescriptor &&fd, int uid, int num);
 
 	~Client() {
 		if (FullyBufferedSocket::IsDefined())
@@ -236,7 +237,7 @@ client_manager_init();
 
 void
 client_new(EventLoop &loop, Partition &partition,
-	   SocketDescriptor fd, SocketAddress address, int uid);
+	   UniqueSocketDescriptor &&fd, SocketAddress address, int uid);
 
 /**
  * Write a printf-like formatted string to the client.

src/client/ClientNew.cxx
@@ -23,6 +23,7 @@
 #include "Partition.hxx"
 #include "Instance.hxx"
 #include "system/fd_util.h"
+#include "net/UniqueSocketDescriptor.hxx"
 #include "net/SocketAddress.hxx"
 #include "net/ToString.hxx"
 #include "Permission.hxx"
@@ -42,8 +43,9 @@
 static const char GREETING[] = "OK MPD " PROTOCOL_VERSION "\n";
 
 Client::Client(EventLoop &_loop, Partition &_partition,
-	       SocketDescriptor _fd, int _uid, int _num)
-	:FullyBufferedSocket(_fd, _loop, 16384, client_max_output_buffer_size),
+	       UniqueSocketDescriptor &&_fd, int _uid, int _num)
+	:FullyBufferedSocket(_fd.Release(), _loop,
+			     16384, client_max_output_buffer_size),
 	 TimeoutMonitor(_loop),
 	 partition(&_partition),
 	 permission(getDefaultPermissions()),
@@ -57,7 +59,7 @@ Client::Client(EventLoop &_loop, Partition &_partition,
 
 void
 client_new(EventLoop &loop, Partition &partition,
-	   SocketDescriptor fd, SocketAddress address, int uid)
+	   UniqueSocketDescriptor &&fd, SocketAddress address, int uid)
 {
 	static unsigned int next_client_num;
 	const auto remote = ToString(address);
@@ -80,7 +82,6 @@ client_new(EventLoop &loop, Partition &partition,
 				      "libwrap refused connection (libwrap=%s) from %s",
 				      progname, remote.c_str());
 
-			fd.Close();
 			return;
 		}
 	}
@@ -89,14 +90,13 @@ client_new(EventLoop &loop, Partition &partition,
 	ClientList &client_list = *partition.instance.client_list;
 	if (client_list.IsFull()) {
 		LogWarning(client_domain, "Max connections reached");
-		fd.Close();
 		return;
 	}
 
-	Client *client = new Client(loop, partition, fd, uid,
-				    next_client_num++);
-
 	(void)fd.Write(GREETING, sizeof(GREETING) - 1);
+
+	Client *client = new Client(loop, partition, std::move(fd), uid,
+				    next_client_num++);
 
 	client_list.Add(*client);
 

src/event/ServerSocket.hxx
@@ -24,6 +24,7 @@
 
 class SocketAddress;
 class AllocatedSocketAddress;
+class UniqueSocketDescriptor;
 class EventLoop;
 class AllocatedPath;
 class OneServerSocket;
@@ -116,7 +117,8 @@ public:
 	void Close();
 
 protected:
-	virtual void OnAccept(int fd, SocketAddress address, int uid) = 0;
+	virtual void OnAccept(UniqueSocketDescriptor &&fd,
+			      SocketAddress address, int uid) = 0;
 };
 
 #endif

src/output/plugins/httpd/HttpdClient.cxx
@@ -25,6 +25,7 @@
 #include "Page.hxx"
 #include "IcyMetaDataServer.hxx"
 #include "net/SocketError.hxx"
+#include "net/UniqueSocketDescriptor.hxx"
 #include "Log.hxx"
 
 #include <assert.h>
@@ -185,10 +186,10 @@ HttpdClient::SendResponse()
 	return true;
 }
 
-HttpdClient::HttpdClient(HttpdOutput &_httpd, SocketDescriptor _fd,
+HttpdClient::HttpdClient(HttpdOutput &_httpd, UniqueSocketDescriptor &&_fd,
 			 EventLoop &_loop,
 			 bool _metadata_supported)
-	:BufferedSocket(_fd, _loop),
+	:BufferedSocket(_fd.Release(), _loop),
 	 httpd(_httpd),
 	 state(REQUEST),
 	 queue_size(0),

src/output/plugins/httpd/HttpdClient.hxx
@@ -32,6 +32,7 @@
 
 #include <stddef.h>
 
+class UniqueSocketDescriptor;
 class HttpdOutput;
 
 class HttpdClient final
@@ -131,7 +132,8 @@ public:
 	 * @param httpd the HTTP output device
 	 * @param _fd the socket file descriptor
 	 */
-	HttpdClient(HttpdOutput &httpd, SocketDescriptor _fd, EventLoop &_loop,
+	HttpdClient(HttpdOutput &httpd, UniqueSocketDescriptor &&_fd,
+		    EventLoop &_loop,
 		    bool _metadata_supported);
 
 	/**

src/output/plugins/httpd/HttpdInternal.hxx
@@ -206,7 +206,7 @@ public:
 		return HasClients();
 	}
 
-	void AddClient(SocketDescriptor fd);
+	void AddClient(UniqueSocketDescriptor &&fd);
 
 	/**
 	 * Removes a client from the httpd_output.clients linked list.
@@ -257,7 +257,8 @@ public:
 private:
 	virtual void RunDeferred() override;
 
-	void OnAccept(int fd, SocketAddress address, int uid) override;
+	void OnAccept(UniqueSocketDescriptor &&fd,
+		      SocketAddress address, int uid) override;
 };
 
 extern const class Domain httpd_output_domain;

commit 33353a2dfafeb3a2281d04dc3eaa9347494ce120

event/ServerSocket: fix get_remote_uid() call
This call was broken since commit
0965524d0a397b776aec2bbb2b10fae50b34b0c4 because the
`UniqueSocketDescriptor` had already been moved.

src/event/ServerSocket.cxx
@@ -163,8 +163,9 @@ OneServerSocket::Accept() noexcept
 			    (const char *)msg);
 	}
 
-	parent.OnAccept(std::move(peer_fd), peer_address,
-			get_remote_uid(peer_fd.Get()));
+	const auto uid = get_remote_uid(peer_fd.Get());
+
+	parent.OnAccept(std::move(peer_fd), peer_address, uid);
 }
 
 bool