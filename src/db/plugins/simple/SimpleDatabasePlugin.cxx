GameKyuubi/MPD

commit 086fa5cbb7befda9c206dbe323a470af6d72c9d6

db/simple: wrap LightSong in Manual<>
Prepare to make LightSong non-assignable.

src/db/plugins/simple/SimpleDatabasePlugin.cxx
@@ -237,30 +237,32 @@ SimpleDatabase::GetSong(const char *uri) const
 		throw DatabaseError(DatabaseErrorCode::NOT_FOUND,
 				    "No such song");
 
-	light_song = song->Export();
+	light_song.Construct(song->Export());
 
 #ifndef NDEBUG
 	++borrowed_song_count;
 #endif
 
-	return &light_song;
+	return &light_song.Get();
 }
 
 void
 SimpleDatabase::ReturnSong(gcc_unused const LightSong *song) const
 {
 	assert(song != nullptr);
-	assert(song == &light_song || song == prefixed_light_song);
+	assert(song == &light_song.Get() || song == prefixed_light_song);
 
-	delete prefixed_light_song;
-	prefixed_light_song = nullptr;
-
+	if (prefixed_light_song != nullptr) {
+		delete prefixed_light_song;
+		prefixed_light_song = nullptr;
+	} else {
 #ifndef NDEBUG
-	if (song == &light_song) {
 		assert(borrowed_song_count > 0);
 		--borrowed_song_count;
-	}
 #endif
+
+		light_song.Destruct();
+	}
 }
 
 void

src/db/plugins/simple/SimpleDatabasePlugin.hxx
@@ -24,6 +24,7 @@
 #include "db/Interface.hxx"
 #include "fs/AllocatedPath.hxx"
 #include "db/LightSong.hxx"
+#include "util/Manual.hxx"
 #include "Compiler.h"
 
 #include <cassert>
@@ -61,7 +62,7 @@ class SimpleDatabase : public Database {
 	/**
 	 * A buffer for GetSong().
 	 */
-	mutable LightSong light_song;
+	mutable Manual<LightSong> light_song;
 
 #ifndef NDEBUG
 	mutable unsigned borrowed_song_count;

commit 4a7989807061505c2906c47d7dd9e27df4b3330d

db/simple: reorder checks in assert() to fix assertion failure
`light_song.Get()` could cause an assertion failure because the
`Manual<>` object must not be used if uninitialized.

Regression by commit 086fa5cbb7befda9c206dbe323a470af6d72c9d6

src/db/plugins/simple/SimpleDatabasePlugin.cxx
@@ -251,7 +251,7 @@ void
 SimpleDatabase::ReturnSong(gcc_unused const LightSong *song) const noexcept
 {
 	assert(song != nullptr);
-	assert(song == &light_song.Get() || song == prefixed_light_song);
+	assert(song == prefixed_light_song || song == &light_song.Get());
 
 	if (prefixed_light_song != nullptr) {
 		delete prefixed_light_song;