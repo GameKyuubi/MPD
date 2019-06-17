GameKyuubi/MPD

commit e70cc54395426ed63fe99d2a8a9cc008076a7c8e

MusicChunk: make the struct size exactly 4096

src/MusicChunk.hxx
@@ -124,7 +124,7 @@ struct MusicChunkInfo {
  */
 struct MusicChunk : MusicChunkInfo {
 	/** the data (probably PCM) */
-	uint8_t data[CHUNK_SIZE];
+	uint8_t data[CHUNK_SIZE - sizeof(MusicChunkInfo)];
 
 	/**
 	 * Prepares appending to the music chunk.  Returns a buffer
@@ -152,5 +152,7 @@ struct MusicChunk : MusicChunkInfo {
 	 */
 	bool Expand(AudioFormat af, size_t length) noexcept;
 };
+
+static_assert(sizeof(MusicChunk) == CHUNK_SIZE, "Wrong size");
 
 #endif

commit 5ab898f6ba8de43e69b6023d6a68933d4a5c5dfc

MusicChunk: pad MusicChunkInfo to a multiple of 8 bytes
Workaround for a regression caused by commit
e70cc54395426ed63fe99d2a8a9cc008076a7c8e, revealing a problem with
discarding odd numer of frames in the DSD_U32 and DoP converters,
causing distortions with DSD_U32 and DoP on 32 bit CPUs.

Closes https://github.com/MusicPlayerDaemon/MPD/issues/469

src/MusicChunk.hxx
@@ -43,7 +43,15 @@ struct MusicChunk;
 /**
  * Meta information for #MusicChunk.
  */
-struct MusicChunkInfo {
+struct alignas(8) MusicChunkInfo {
+	/* align to multiple of 8 bytes, which adds padding at the
+	   end, so the size of MusicChunk::data is also a multiple of
+	   8 bytes; this is a workaround for a bug in the DSD_U32 and
+	   DoP converters which require processing 8 bytes at a time,
+	   discarding the remainder */
+	/* TODO: once all converters have been fixed, we should remove
+	   this workaround */
+
 	/** the next chunk in a linked list */
 	MusicChunkPtr next;
 
@@ -118,6 +126,10 @@ struct MusicChunkInfo {
 struct MusicChunk : MusicChunkInfo {
 	/** the data (probably PCM) */
 	uint8_t data[CHUNK_SIZE - sizeof(MusicChunkInfo)];
+
+	/* TODO: remove this check once all converters have been fixed
+	   (see comment in struct MusicChunkInfo for details) */
+	static_assert(sizeof(data) % 8 == 0, "Wrong alignment");
 
 	/**
 	 * Prepares appending to the music chunk.  Returns a buffer