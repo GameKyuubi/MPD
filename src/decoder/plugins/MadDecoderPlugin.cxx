GameKyuubi/MPD

commit aa772eb14bce919750418e3d87e56521e356d21b

Input*: move to input/

src/decoder/plugins/MadDecoderPlugin.cxx
@@ -20,7 +20,7 @@
 #include "config.h"
 #include "MadDecoderPlugin.hxx"
 #include "../DecoderAPI.hxx"
-#include "InputStream.hxx"
+#include "input/InputStream.hxx"
 #include "config/ConfigGlobal.hxx"
 #include "tag/TagId3.hxx"
 #include "tag/TagRva2.hxx"
@@ -1096,7 +1096,7 @@ mp3_decode(Decoder &decoder, InputStream &input_stream)
 
 		if (decoder_get_command(decoder) == DecoderCommand::NONE)
 			LogError(mad_domain,
-				 "Input does not appear to be a mp3 bit stream");
+				 "input/Input does not appear to be a mp3 bit stream");
 		return;
 	}
 

commit 5f84d70efb71630dc23639579983b32cb1e8644b

decoder/mad: fix broken log message
Broken since commit aa772eb14bce919750418e3d87e56521e356d21b

src/decoder/plugins/MadDecoderPlugin.cxx
@@ -969,7 +969,7 @@ mad_decode(DecoderClient &client, InputStream &input_stream)
 	if (!data.DecodeFirstFrame(&tag)) {
 		if (client.GetCommand() == DecoderCommand::NONE)
 			LogError(mad_domain,
-				 "input/Input does not appear to be a mp3 bit stream");
+				 "input does not appear to be a mp3 bit stream");
 		return;
 	}
 