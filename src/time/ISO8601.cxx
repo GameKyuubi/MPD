GameKyuubi/MPD

commit 2aec535ac4083e97975e59d529cb554b72539511

time/ISO8601: move code to ParseTimeOfDay()

src/time/ISO8601.cxx
@@ -108,6 +108,30 @@ ParseTimeZoneOffset(const char *&s)
 	return d;
 }
 
+static const char *
+ParseTimeOfDay(const char *s, struct tm &tm,
+	       std::chrono::system_clock::duration &precision) noexcept
+{
+	const char *end;
+
+	if ((end = strptime(s, "%T", &tm)) != nullptr)
+		precision = std::chrono::seconds(1);
+	else if ((end = strptime(s, "%H%M%S", &tm)) != nullptr)
+		/* no field separators */
+		precision = std::chrono::seconds(1);
+	else if ((end = strptime(s, "%H%M", &tm)) != nullptr)
+		/* no field separators */
+		precision = std::chrono::minutes(1);
+	else if ((end = strptime(s, "%H:%M", &tm)) != nullptr)
+		precision = std::chrono::minutes(1);
+	else if ((end = strptime(s, "%H", &tm)) != nullptr)
+		precision = std::chrono::hours(1);
+	else
+		return nullptr;
+
+	return end;
+}
+
 std::pair<std::chrono::system_clock::time_point,
 	  std::chrono::system_clock::duration>
 ParseISO8601(const char *s)
@@ -138,22 +162,9 @@ ParseISO8601(const char *s)
 	if (*s == 'T') {
 		++s;
 
-		if ((end = strptime(s, "%T", &tm)) != nullptr)
-			precision = std::chrono::seconds(1);
-		else if ((end = strptime(s, "%H%M%S", &tm)) != nullptr)
-			/* no field separators */
-			precision = std::chrono::seconds(1);
-		else if ((end = strptime(s, "%H%M", &tm)) != nullptr)
-			/* no field separators */
-			precision = std::chrono::minutes(1);
-		else if ((end = strptime(s, "%H:%M", &tm)) != nullptr)
-			precision = std::chrono::minutes(1);
-		else if ((end = strptime(s, "%H", &tm)) != nullptr)
-			precision = std::chrono::hours(1);
-		else
+		s = ParseTimeOfDay(s, tm, precision);
+		if (s == nullptr)
 			throw std::runtime_error("Failed to parse time of day");
-
-		s = end;
 	}
 
 	auto tp = TimeGm(tm);

commit 15ced3a82bbbad58cd563a6527760c2d9c921323

time/ISO8601: fix Windows build failure
Caused by 2aec535ac4083e97975e59d529cb554b72539511

src/time/ISO8601.cxx
@@ -58,6 +58,8 @@ FormatISO8601(std::chrono::system_clock::time_point tp)
 	return FormatISO8601(GmTime(tp));
 }
 
+#ifndef _WIN32
+
 static std::pair<unsigned, unsigned>
 ParseTimeZoneOffsetRaw(const char *&s)
 {
@@ -166,6 +168,8 @@ ParseTimeOfDay(const char *s, struct tm &tm,
 	precision = std::chrono::seconds(1);
 	return end;
 }
+
+#endif
 
 std::pair<std::chrono::system_clock::time_point,
 	  std::chrono::system_clock::duration>