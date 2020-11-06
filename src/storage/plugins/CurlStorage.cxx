GameKyuubi/MPD

commit 075f2b9c4a1fec5e8b7a057f42d48feccdb8741e

storage/curl: unescape URI in MapToRelativeUTF8()

src/storage/plugins/CurlStorage.cxx
@@ -85,9 +85,8 @@ CurlStorage::MapUTF8(const char *uri_utf8) const noexcept
 const char *
 CurlStorage::MapToRelativeUTF8(const char *uri_utf8) const noexcept
 {
-	// TODO: escape/unescape?
-
-	return PathTraitsUTF8::Relative(base.c_str(), uri_utf8);
+	return PathTraitsUTF8::Relative(base.c_str(),
+					CurlUnescape(uri_utf8).c_str());
 }
 
 class BlockingHttpRequest : protected CurlResponseHandler {

commit a18ca3277e943464bf87948e081ba29c9c3e93e5

storage/curl: fix nullptr dereference
Pass a std::string to PathTraitsUTF8::Relative(), implicitly casting
it to std::string_view.  This selects the right overload which returns
std::string_view instead of `const char *`; the latter could return
`nullptr` which would cause the implicit conversion of the return
value to std::string_view to crash.

Regression caused by commits 075f2b9c4a1 and ef6f5b589e5.

Closes https://github.com/MusicPlayerDaemon/MPD/issues/995

src/storage/plugins/CurlStorage.cxx
@@ -80,7 +80,7 @@ std::string_view
 CurlStorage::MapToRelativeUTF8(std::string_view uri_utf8) const noexcept
 {
 	return PathTraitsUTF8::Relative(base,
-					CurlUnescape(uri_utf8).c_str());
+					CurlUnescape(uri_utf8));
 }
 
 class BlockingHttpRequest : protected CurlResponseHandler {