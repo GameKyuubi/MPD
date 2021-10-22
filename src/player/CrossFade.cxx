GameKyuubi/MPD

commit 70e115d630b7b44e2a196f55de1fcb6135eaebe6

player/CrossFade: move code to CanCrossFadeSong()

src/player/CrossFade.cxx
@@ -30,6 +30,13 @@
 
 static constexpr Domain cross_fade_domain("cross_fade");
 
+inline bool
+CrossFadeSettings::CanCrossFadeSong(SignedSongTime total_time) const noexcept
+{
+	return !total_time.IsNegative() &&
+		duration >= std::chrono::duration_cast<FloatDuration>(total_time);
+}
+
 gcc_pure
 static FloatDuration
 mixramp_interpolate(const char *ramp_list, float required_db) noexcept
@@ -97,8 +104,7 @@ CrossFadeSettings::Calculate(SignedSongTime total_time,
 	unsigned int chunks = 0;
 
 	if (!IsEnabled() ||
-	    total_time.IsNegative() ||
-	    duration >= std::chrono::duration_cast<FloatDuration>(total_time) ||
+	    !CanCrossFadeSong(total_time) ||
 	    /* we can't crossfade when the audio formats are different */
 	    af != old_format)
 		return 0;

src/player/CrossFade.hxx
@@ -64,6 +64,13 @@ struct CrossFadeSettings {
 			   const char *mixramp_prev_end,
 			   AudioFormat af, AudioFormat old_format,
 			   unsigned max_chunks) const noexcept;
+
+private:
+	/**
+	 * Can the described song be cross-faded?
+	 */
+	[[gnu::pure]]
+	bool CanCrossFadeSong(SignedSongTime total_time) const noexcept;
 };
 
 #endif

commit a4b490f566516f0dc7cb719fa55a05db0880a36e

player/CrossFade: fix inverted check and wrong variable
The inverted check was introduced by commit 70e115d630b, and commit
5ca2ca992b added a check for the wrong variable.  D'oh!

Closes https://github.com/MusicPlayerDaemon/MPD/issues/1303

src/player/CrossFade.cxx
@@ -34,8 +34,8 @@ inline bool
 CrossFadeSettings::CanCrossFadeSong(SignedSongTime total_time) const noexcept
 {
 	return !total_time.IsNegative() &&
-		duration >= MIN_TOTAL_TIME &&
-		duration >= std::chrono::duration_cast<FloatDuration>(total_time);
+		total_time >= MIN_TOTAL_TIME &&
+		duration < std::chrono::duration_cast<FloatDuration>(total_time);
 }
 
 gcc_pure